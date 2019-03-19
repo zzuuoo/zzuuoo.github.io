92c9d202ce6eb5ea666a3984c94025662155d539
zuoweixing <zuoweixing@guazi.com>
2019-03-19 17:30:43 +0800

    应用别人的博客代码

---
+++ config.js
@@ -0,0 +1,20 @@
+window.config = {
+  server: 'https://api.github.com',
+  siteName: 'zuoVx',
+  navbar: [
+    {
+      label: '首页',
+      link: '/'
+    }, {
+      label: '博客',
+      link: '/blog'
+    }, {
+      label: 'react',
+      link: '/blog/tag/react'
+    }
+  ],
+  github: {
+    user: 'zzuuoo',
+    repo: 'blog'
+  }
+}
